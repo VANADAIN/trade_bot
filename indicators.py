dddad6ab08dc40c454f1c6edaff652dfeda3d94c
Ivan Marchuk <ivan.marchuk@mail.ru>
2021-05-23 00:10:59 +0300

    switched to simple rsa ma strategy

--- indicators.py
+++ indicators.py
@@ -1,7 +1,7 @@
 
 from ta.volatility import AverageTrueRange
-from ta.momentum import RSIIndicator, ROCIndicator
-from ta.trend import CCIIndicator
+from ta.momentum import RSIIndicator, ROCIndicator, StochRSIIndicator
+from ta.trend import CCIIndicator, EMAIndicator
 from exchange import Data
 import warnings
 warnings.filterwarnings('ignore')
@@ -9,149 +9,218 @@ warnings.filterwarnings('ignore')
 
 class SuperTrend:
 
-	def average_true_range(self, df, atr_window=50):
-		atr_ta = AverageTrueRange(df['high'], df['low'],
-		                          df['close'], window=atr_window)
-		df['atr'] = atr_ta.average_true_range()
+    def average_true_range(self, df, atr_window=50):
+        atr_ta = AverageTrueRange(df['high'], df['low'],
+                                  df['close'], window=atr_window)
 
-		return df
+        name = "atr_" + str(atr_window)
+        df[name] = atr_ta.average_true_range()
 
-	def supertrend(self, df, multiplier=1):
-		hl2 = (df['high'] + df['low']) / 2
-		df['upperband'] = hl2 + (multiplier * df['atr'])
-		df['lowerband'] = hl2 - (multiplier * df['atr'])
-		df['in_uptrend'] = True
+        return df, atr_window
 
-		for current in range(1, len(df.index)):
-			previous = current - 1
-			if df['close'][current] > df['upperband'][previous]:
-				df['in_uptrend'][current] = True
-			elif df['close'][current] < df['lowerband'][previous]:
-				df['in_uptrend'][current] = False
-			else:
-				df['in_uptrend'][current] = df['in_uptrend'][previous]
+    def supertrend(self, df, atr, multiplier=1):
+        hl2 = (df['high'] + df['low']) / 2
+        atr_name = "atr_" + str(atr)
 
-				if df['in_uptrend'][current] and df['lowerband'][current] < df['lowerband'][previous]:
-					df['lowerband'][current] = df['lowerband'][previous]
+        uband = "uband_" + str(atr)
+        lband = "lband_" + str(atr)
+        uptrend = "in_uptrend_" + str(atr)
 
-				if not df['in_uptrend'][current] and df['upperband'][current] > df['upperband'][previous]:
-					df['upperband'][current] = df['upperband'][previous]
+        df[uband] = hl2 + (multiplier * df[atr_name])
+        df[lband] = hl2 - (multiplier * df[atr_name])
+        df[uptrend] = True
 
-		return df
+        for current in range(1, len(df.index)):
+            previous = current - 1
+            if df['close'][current] > df[uband][previous]:
+                df[uptrend][current] = True
+            elif df['close'][current] < df[lband][previous]:
+                df[uptrend][current] = False
+            else:
+                df[uptrend][current] = df[uptrend][previous]
 
-	def create_signal(self, df):
+                if df[uptrend][current] and df[lband][current] < df[lband][previous]:
+                    df[lband][current] = df[lband][previous]
 
-		if not df['in_uptrend'].iloc[-2] and df['in_uptrend'].iloc[-1]:
+                if not df[uptrend][current] and df[uband][current] > df[uband][previous]:
+                    df[uband][current] = df[uband][previous]
 
-			# buy signal
-			return 1
+        df.drop([uband, lband], axis=1, inplace=True)
 
-		if df['in_uptrend'].iloc[-2] and not df['in_uptrend'].iloc[-1]:
+        return df
 
-			# sell signal
-			return -1
+    def create_signal(self, df, atr):
 
-		else:
-			if df['in_uptrend'].iloc[-2] and df['in_uptrend'].iloc[-1]:
+        uptrend = "in_uptrend_" + str(atr)
 
-			# (True - True) -> in a buy trend
-				return 0.5
+        if not df[uptrend].iloc[-2] and df[uptrend].iloc[-1]:
 
-			if not df['in_uptrend'].iloc[-2] and not df['in_uptrend'].iloc[-1]:
+            # buy signal
+            return 1
 
-			# (False - False) -> in a sell trend
-				return -0.5
+        if df[uptrend].iloc[-2] and not df[uptrend].iloc[-1]:
+
+            # sell signal
+            return -1
+
+        else:
+            if df[uptrend].iloc[-2] and df[uptrend].iloc[-1]:
+
+                # (True - True) -> in a buy trend
+                return 0.5
+
+            if not df[uptrend].iloc[-2] and not df[uptrend].iloc[-1]:
+
+                # (False - False) -> in a sell trend
+                return -0.5
 
 
 class CCI:
 
-	def calculate_cci(self, df):
-		cci = CCIIndicator(df['high'], df['low'], df['close'], window=10)
-		df['cci'] = cci.cci()
+    def calculate_cci(self, df):
+        cci = CCIIndicator(
+            df['high'], df['low'], df['close'], window=10)
+        df['cci'] = cci.cci()
 
-		return df
+        return df
 
-	def create_signal(self, df):
+    def create_signal(self, df):
 
-		if df['cci'].iloc[-1] > 100:
-			# if current is less then previous (cci downtrend)
-			if df['cci'].iloc[-1] < df['cci'].iloc[-2]:
+        if df['cci'].iloc[-1] > 100:
+            # if current is less then previous (cci downtrend)
+            if df['cci'].iloc[-1] < df['cci'].iloc[-2]:
+                return 0
 
-				return 0
+            else:
+                return 1
 
-			else:
+        elif df['cci'].iloc[-1] < -100:
+            return -1
 
-				return 1
-
-		elif df['cci'].iloc[-1] < -100:
-
-			return -1
-
-		# in channel
-		else:
-
-			return 0
+        # in channel
+        else:
+            return 0
 
 
 class RSI:
 
-	def calculate_rsi(self, df, period):
-		rsi_ind = RSIIndicator(df['close'], window=period)
-		rsi = rsi_ind.rsi()
-		df['rsi'] = rsi
+    def calculate_rsi(self, df, period):
+        rsi_ind = RSIIndicator(df['close'], window=period)
+        rsi = rsi_ind.rsi()
+        df['rsi'] = rsi
 
-		return df
+        return df
 
-	def calculate_smas(self, df, w1, w2):
-		# short ma
-		df['rsi_short'] = df['rsi'].rolling(window=w1).mean()
+    def calculate_smas(self, df, w1, w2):
+        # short ma
+        df['rsi_short'] = df['rsi'].rolling(window=w1).mean()
 
-		# long ma
-		df['rsi_long'] = df['rsi'].rolling(window=w2).mean()
+        # long ma
+        df['rsi_long'] = df['rsi'].rolling(window=w2).mean()
 
-		return df
+        return df
 
-	def create_ma_signal(self, df):
+    def create_ma_signal(self, df):
 
-		treshold = 7
-		rsi_delta = df['rsi'].iloc[-1] - df['rsi'].iloc[-2]
-		delta_w_ma = df['rsi'].iloc[-1] - df['rsi_short'].iloc[-1]
+        s1 = df['rsi_short'].iloc[-1]
+        l1 = df['rsi_long'].iloc[-1]
 
-		if df['rsi'].iloc[-1] < df['rsi_short'].iloc[-1]:
-		    return -1
+        s2 = df['rsi_short'].iloc[-1]
+        l2 = df['rsi_long'].iloc[-1]
 
-		elif df['rsi'].iloc[-1] > df['rsi_short'].iloc[-1] and rsi_delta > treshold and delta_w_ma > treshold:
-			return 1
+        uptrend = self.average_trend(df)
 
-		else:
-		    return 0
-	    
+        # cross down -> sell
+        if s2 > l2 and s1 < l1:
+            return -1
+
+        elif uptrend == True and s1 > l1 and s2 < l2:
+            return 1
+
+        else:
+            return 0
+
+    def average_trend(self, df):
+
+        previous = df['rsi_long'].iloc[-11:-6]
+        now = df['rsi_long'].iloc[-5:-1]
+
+        print(f' PRev sum : {previous.sum()}')
+        print(f' Now sum : {now.sum()}')
+
+        if previous.sum() > now.sum():
+            return False
+        else:
+            return True
+
+
 class ROC:
 
-	def calculate_roc(self, df, window):
+    def calculate_roc(self, df, window):
 
-		roc = ROCIndicator(df['close'], window = window)
-		df['roc'] = roc.roc() 
+        roc = ROCIndicator(df['close'], window=window)
+        df['roc'] = roc.roc()
 
-		return df
+        return df
 
-	def create_delta_signal(self, df):
-		
-		treshold = 1
-		delta = df['roc'].iloc[-1] - df['roc'].iloc[-2] 
+    def create_delta_signal(self, df):
 
-		if delta > treshold:
-			return 1
-		else: 
-			return -1
+        treshold = 1
+        delta = df['roc'].iloc[-1] - df['roc'].iloc[-2]
 
-
-class ZZ:
-
-	def __init__(self):
-		
-		self.rsi_last_state = 0
+        if delta > treshold:
+            return 1
+        else:
+            return -1
 
 
-	def calculate(self):
-		pass
+class S_RSI:
+
+    def calculate(self, df, window, sm1, sm2):
+
+        srsi = StochRSIIndicator(
+            df['close'], window=14, smooth1=sm1, smooth2=sm2)
+        df['s_rsi_d'] = srsi.stochrsi_d()
+        df['s_rsi_k'] = srsi.stochrsi_k()
+
+        return df
+
+    def create_signal(self, df):
+        ob_level = 0.80
+        os_level = 0.20
+
+        d = df['s_rsi_d'].iloc[-1]
+        k = df['s_rsi_k'].iloc[-1]
+
+        d2 = df['s_rsi_d'].iloc[-2]
+        k2 = df['s_rsi_k'].iloc[-2]
+
+        # k must cross d -> cross from above
+        if d > ob_level and k > ob_level and k2 > d2 and k1 < d1:
+
+            return -1
+
+        elif d < os_level and k < os_level and k2 < d2 and k1 > d1:
+
+            return 1
+
+        else:
+            return 0
+
+
+class EMA:
+
+    def calculate(self, df, window):
+        ema = EMAIndicator(df['close'], window=window)
+
+        df['ema'] = ema.ema_indicator()
+
+        return df
+
+    def create_signal(self, df):
+
+        if df['close'].iloc[-1] > df['ema'].iloc[-1]:
+            return 1
+
+        else:
+            return -1
--- trader.py
+++ trader.py
@@ -2,186 +2,200 @@
 import json
 import time
 import schedule
-from datetime import datetime 
+from datetime import datetime
 from colorama import Fore
 from telegram import Notifier
 from exchange import Data
-from indicators import SuperTrend, CCI, RSI, ROC
+from indicators import SuperTrend, CCI, RSI, ROC, S_RSI, EMA
 from volume_indicators import Volume, VWAP
+
 
 class Trader:
 
-	def __init__(self):
-		self.data = Data()
-		self.notifier = Notifier()
+    def __init__(self):
+        self.data = Data()
+        self.notifier = Notifier()
 
-		self.volume_ind = Volume()
-		self.vwap_ind = VWAP()
+        # self.volume_ind = Volume()
+        # self.vwap_ind = VWAP()
 
-		self.st_ind = SuperTrend()
-		self.cci_ind = CCI()
-		self.rsi_ind = RSI()
-		self.roc_ind = ROC()
+        # self.st_ind = SuperTrend()
+        # self.ema_ind = EMA()
+        # self.srsi_ind = S_RSI()
 
-		self.signals = {}
+        # self.cci_ind = CCI()
+        self.rsi_ind = RSI()
+        # self.roc_ind = ROC()
 
-		self.bought_amount = 0
-		self.bought_for = 0
-		self.sold_for = 0
+        self.signals = {}
 
-		self.IN_POSITION = False
+        self.bought_amount = 0
+        self.bought_for = 0
+        self.sold_for = 0
 
-	def get_signals(self, period, frame):
-		df = self.data.get_candle_data(period, frame)
-		df = self.st_ind.average_true_range(df, 50)
-		df = self.st_ind.supertrend(df, multiplier = 1.8)
-		df = self.cci_ind.calculate_cci(df)
-		df = self.rsi_ind.calculate_rsi(df, 10)
-		df = self.rsi_ind.calculate_smas(df, 10, 12)
-		df = self.roc_ind.calculate_roc(df, 10)
-		df = self.vwap_ind.calculate_vwap(df, 50)
+        self.IN_POSITION = False
 
+    def get_signals(self, period, frame):
+        df = self.data.get_candle_data(period, frame)
 
-		st_signal = self.st_ind.create_signal(df)
-		vbs_signal = self.volume_ind.create_bs_signal(df, 20)
-		cci_signal = self.cci_ind.create_signal(df)
-		rsi_ma_signal = self.rsi_ind.create_ma_signal(df)
-		roc_signal = self.roc_ind.create_delta_signal(df)
-		vwap_signal = self.vwap_ind.create_signal(df)
+        # df, atr_1 = self.st_ind.average_true_range(df, 12)
+        # df = self.st_ind.supertrend(df, atr_1, multiplier=3)
+        # df = self.cci_ind.calculate_cci(df)
+        # df = self.roc_ind.calculate_roc(df, 10)
+        # df = self.vwap_ind.calculate_vwap(df, 50)
 
-		self.signals['supertrend'] = st_signal
-		self.signals['volume_bs'] = vbs_signal
-		self.signals['cci'] = cci_signal
-		self.signals['rsi_ma'] = rsi_ma_signal
-		self.signals['roc_delta'] = roc_signal
-		self.signals['vwap'] = vwap_signal
+        df = self.rsi_ind.calculate_rsi(df, 10)
+        df = self.rsi_ind.calculate_smas(df, 3, 10)
 
-		return df
+        # st_signal_1 = self.st_ind.create_signal(df, atr_1)
+        # st_signal_2 = self.st_ind.create_signal(df, atr_2)
+        # st_signal_3 = self.st_ind.create_signal(df, atr_3)
+        # ema_signal = self.ema_ind.create_signal(df)
+        # srsi_signal = self.srsi_ind.create_signal(df)
+        # vbs_signal = self.volume_ind.create_bs_signal(df, 20)
+        # cci_signal = self.cci_ind.create_signal(df)
+        # roc_signal = self.roc_ind.create_delta_signal(df)
+        # vwap_signal = self.vwap_ind.create_signal(df)
 
-	def get_available_amount(self, df):
-		actual_price = df['close'].iloc[-1] 
-		price_for_amount = actual_price + 20.0
-		usdt_data = trader.data.get_balance('USDT')
-		usdt_free = usdt_data['free']
-		amount = usdt_free / price_for_amount
-		amount = amount / 2
-		
-		return actual_price, amount
+        rsi_ma_signal = self.rsi_ind.create_ma_signal(df)
 
-	def decision_maker(self):
-		
-		# if self.signals['supertrend'] == 1 and self.signals['rsi_ma'] == 1 and self.signals['roc_delta'] == 1 and self.signals['volume_bs'] == 1 and self.signals['vwap'] == 1 : 
-		
+        # self.signals['supertrend_12_3'] = st_signal_1
+        # self.signals['supertrend_11_2'] = st_signal_2
+        # self.signals['supertrend_10_1'] = st_signal_3
+        # self.signals['ema'] = ema_signal
+        # self.signals['srsi'] = srsi_signal
+        # self.signals['volume_bs'] = vbs_signal
+        # self.signals['cci'] = cci_signal
 
-			return 'buy'
+        self.signals['rsi_ma'] = rsi_ma_signal
 
-		elif  self.signals['supertrend'] == -1 :
-		
-			return 'sell'
+        # self.signals['roc_delta'] = roc_signal
+        # self.signals['vwap'] = vwap_signal
 
-		else: 
+        return df
 
-			return 'wait'
+    def get_available_amount(self, df):
+        actual_price = df['close'].iloc[-1]
+        price_for_amount = actual_price + 20.0
+        usdt_data = trader.data.get_balance('USDT')
+        usdt_free = usdt_data['free']
+        amount = usdt_free / price_for_amount
+        amount = 100
 
-	def trade(self, decision, price, amount): 
+        return actual_price, amount
 
+    def decision_maker(self):
 
-		if decision == 'buy' and self.IN_POSITION == False:
+        if self.signals['rsi_ma'] == 1:
 
-			self.buy(price, amount)
-			self.IN_POSITION = True
-			
-		elif decision == 'sell' and self.IN_POSITION == True:
+            return 'buy'
 
-			self.sell(price)
-			self.IN_POSITION = False
+        elif self.signals['rsi_ma'] == -1:
 
-		else:
+            return 'sell'
 
-			pass
+        else:
 
-	def buy(self, price, amount): 
-		order = data.exchange.create_market_buy_order('ETH/USDT', amount)
-		
-		# save order
-		# self.write_order(order, "Buy_")
+            return 'wait'
 
-		# write amount of coin 
-		self.bought_amount = amount
-		self.bought_for = price
+    def trade(self, decision, price, amount):
 
-		print(self.signals)
+        if decision == 'buy' and self.IN_POSITION == False:
 
-		# notify
-		self.notifier.send_message(f'Bought {amount} ETH for {price} $\nTime : {str(datetime.now())}')
+            self.buy(price, amount)
+            self.IN_POSITION = True
 
-	def sell(self, price):
-		order = self.data.create_market_sell_order('ETH/USDT', self.bought_amount)
+        elif decision == 'sell' and self.IN_POSITION == True:
 
-		# self.write_order(order, "Sell_")
+            self.sell(price)
+            self.IN_POSITION = False
 
-		self.sold_for = price
+        else:
 
-		print(self.signals)
+            pass
 
-		self.notifier.send_message(f'Sold {self.bought_amount} ETH for {price} $\nTime : {str(datetime.now())}')
+    def buy(self, price, amount):
+        # order = data.exchange.create_market_buy_order('ETH/USDT', amount)
 
-		self.estimated_income_notifier()
+        # save order
+        # self.write_order(order, "Buy_")
 
+        # write amount of coin
+        self.bought_amount = amount
+        self.bought_for = price
 
-	# def write_order(self, order, order_type):
-	# 	path = 'trade_data/'
-	# 	now = str(datetime.now())
-	# 	filename = path + order_type + now + ".json"
-	# 	out_file = open(filename, "w")
-	# 	json.dump(order, out_file, indent = 4)
-	# 	out_file.close() 
+        print(self.signals)
 
-	def estimated_income_notifier(self):
-		# price in usdt
-		buy = self.bought_for
-		sell = self.sold_for
-		income_pure = sell - buy
-		income = income_pure * self.bought_amount
-		income_rub = income * 75
+        # notify
+        self.notifier.send_message(
+            f'Bought {amount} ETH for {price} $\nTime : {str(datetime.now())}')
 
-		if sell > buy:
-			text = f'Ммм, чувствую запах денег...\n + {income} $\n + {income_rub} Р. (примерно)'
-		else:
-			text = f'Ошибочка :(\n  {income} $\n  {income_rub} Р. (примерно)'
+    def sell(self, price):
+        # order = self.data.create_market_sell_order('ETH/USDT', self.bought_amount)
 
-		self.notifier.send_message(text)
+        # self.write_order(order, "Sell_")
+
+        self.sold_for = price
+
+        print(self.signals)
+
+        self.notifier.send_message(
+            f'Sold {self.bought_amount} ETH for {price} $\nTime : {str(datetime.now())}')
+
+        self.estimated_income_notifier()
+
+    # def write_order(self, order, order_type):
+    # 	path = 'trade_data/'
+    # 	now = str(datetime.now())
+    # 	filename = path + order_type + now + ".json"
+    # 	out_file = open(filename, "w")
+    # 	json.dump(order, out_file, indent = 4)
+    # 	out_file.close()
+
+    def estimated_income_notifier(self):
+        # price in usdt
+        buy = self.bought_for
+        sell = self.sold_for
+        income_pure = sell - buy
+        income = income_pure * self.bought_amount
+        income_rub = income * 75
+
+        if sell > buy:
+            text = f'Ммм, чувствую запах денег...\n + {income} $\n + {income_rub} Р. (примерно)'
+        else:
+            text = f'Ошибочка :(\n  {income} $\n  {income_rub} Р. (примерно)'
+
+        self.notifier.send_message(text)
 
 
 trader = Trader()
 print('Trading now...')
 
+
 def run():
-	# to run on schedule 
-	
-	try:
-    
-		df = trader.get_signals(150, '5m')
-		print(f'Time: {str(datetime.now().time())}\n  sig: {trader.signals}\n')
-		price, amount = trader.get_available_amount(df)
-		decision = trader.decision_maker()
-		trader.trade(decision, price, amount)
+    # to run on schedule
 
-	except Exception as e:
-		trader.notifier.send_message("Exception in code occured:")
-		trader.notifier.send_message(e)
-		trader.notifier.send_message("Check exchange manually")
-		print(e)
-		exit()
+    try:
 
-schedule.every(1).minutes.do(run)
+        df = trader.get_signals(50, '5m')
+        print(df.tail(5))
+        print(f'Time: {str(datetime.now().time())}\n  sig: {trader.signals}\n')
+        price, amount = trader.get_available_amount(df)
+        decision = trader.decision_maker()
+        trader.trade(decision, price, amount)
+
+    except Exception as e:
+        trader.notifier.send_message("Exception in code occured:")
+        trader.notifier.send_message(e)
+        trader.notifier.send_message("Check exchange manually")
+        print(e)
+        exit()
+
+
+# for 5 mins
+schedule.every(5).minutes.at(":07").do(run)
+# schedule.every().minute.at(":05").do(run)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
-
-
-
-
-
-
